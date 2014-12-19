bfb29f7d5e567897946c19c35a985c231aa134ad
Alexandr Nikitin <alexandr.nikitin@adform.com>
2014-12-19 11:42:59 +0200

    Added sample code

--- src/Topshelf.Unity.Sample/Program.cs
+++ src/Topshelf.Unity.Sample/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+
+using Microsoft.Practices.Unity;
 
 namespace Topshelf.Unity.Sample
 {
@@ -9,6 +8,56 @@ namespace Topshelf.Unity.Sample
     {
         static void Main(string[] args)
         {
+            // Create your container
+            var container = new UnityContainer();
+            container.RegisterType<ISampleDependency, SampleDependency>();
+            container.RegisterType<SampleService>();
+
+            HostFactory.Run(c =>
+            {
+                // Pass it to Topshelf
+                c.UseUnityContainer(container);
+
+                c.Service<SampleService>(s =>
+                {
+                    // Let Topshelf use it
+                    s.ConstructUsingUnityContainer();
+                    s.WhenStarted((service, control) => service.Start());
+                    s.WhenStopped((service, control) => service.Stop());
+                });
+            });
+
         }
     }
+
+    public class SampleService
+    {
+        private readonly ISampleDependency _sample;
+
+        public SampleService(ISampleDependency sample)
+        {
+            _sample = sample;
+        }
+
+        public bool Start()
+        {
+            Console.WriteLine("Sample Service Started.");
+            Console.WriteLine("Sample Dependency: {0}", _sample);
+            return _sample != null;
+        }
+
+        public bool Stop()
+        {
+            return _sample != null;
+        }
+    }
+
+    public interface ISampleDependency
+    {
+    }
+
+    public class SampleDependency : ISampleDependency
+    {
+    }
+
 }
