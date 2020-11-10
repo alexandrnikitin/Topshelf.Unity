2efba6963d382be0a28a2b6319795da6183a8739
Kevin Street <Kevin.Street@LighthouseSystems.com>
2020-11-10 14:28:58 +0000

    Multi-targeting net452 and netstandard2.0
     - Updated Topshelf.Unity project to target net452 and netstandard2.0
     - Increased minimum version of Topshelf to 4.2.1
     - Increased minimum version of Unity.Abstractions to 4.1.0
     - Updated build scripts to use 'dotnet' instead of 'msbuild'

--- build/PushPackage.bat
+++ build/PushPackage.bat
@@ -1,1 +1,1 @@
-..\.nuget\NuGet.exe push ..\src\Topshelf.Unity\bin\Release\Topshelf.Unity.*.nupkg -Source https://www.nuget.org/api/v2/package
+dotnet nuget push ..\src\Topshelf.Unity\bin\Release\Topshelf.Unity.*.nupkg -source https://www.nuget.org/api/v2/package
