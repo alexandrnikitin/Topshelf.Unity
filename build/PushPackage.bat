94731823af4e176e5e02cd304bc81fb245fcfe5c
Kevin Street <kev.c.w.street@googlemail.com>
2020-11-11 14:20:05 +0000

    Correct 'source' argument in PushPackage.bat

--- build/PushPackage.bat
+++ build/PushPackage.bat
@@ -1,1 +1,1 @@
-dotnet nuget push ..\src\Topshelf.Unity\bin\Release\Topshelf.Unity.*.nupkg -source https://www.nuget.org/api/v2/package
+dotnet nuget push ..\src\Topshelf.Unity\bin\Release\Topshelf.Unity.*.nupkg --source https://www.nuget.org/api/v2/package
